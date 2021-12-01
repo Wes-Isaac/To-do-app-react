2f2b049d91ca0ba217c19f1ef7944dc205f80d89
unknown <yisehakmitiku@gmail.com>
2021-12-01 16:06:49 +0800

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
-      <Form />
       <ToDoList />
     </div>
   );
-} 
+}
 
 export default App;
