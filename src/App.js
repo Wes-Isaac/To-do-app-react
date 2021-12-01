890e2e8b81bcc5b17203b4568c1eeccf75809ecd
unknown <yisehakmitiku@gmail.com>
2021-12-01 14:54:31 +0800

    Create form and listitem components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="App">
+      <Form />
       <ToDoList />
     </div>
   );
